a8cfb89802a24cf43002f001828ecf4f850ab520
EhGay27 <129858909+EhGay27@users.noreply.github.com>
2023-06-04 14:23:21 +0630

    README

---
+++ EhGay/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "Hi, This is my first step of learning php!";
+echo "<br/>";
+$a = 1;
+var_dump($a);
+echo "<br/>";
+$b = 1.5;
+var_dump($b);
+echo "<br/>";
+$c = "php is cool programming language.";
+var_dump($c);
+echo "<br/>";
+$d = true;
+var_dump($d);
+?>
+
+
+//define('GREETING', 'Hello', true);
+//echo GREETING;
+//echo greeting;
+//echo GrEeTiNg;
+
+define('GREETING', 'Hello', false);
+//echo GREETING;
+echo greeting;
